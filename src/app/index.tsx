b12b6cda7060acbe2c7c024c379160632ba2b145
unknown <iamadelowotan@gmail.com>
2025-01-04 16:23:37 +0100

    update: landing page designed

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,13 @@
 import { Link } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import logo from "../assets/woteva.png";
+import banner1 from "../assets/top-view-delicious-groceries-paper-bag - Copy 1.jpg";
 
 export default function Page() {
   return (
-    <View className="flex flex-1">
+    <View>
       <Header />
       <Content />
       <Footer />
@@ -15,30 +17,27 @@ export default function Page() {
 
 function Content() {
   return (
-    <View className="flex-1">
-      <View className="py-12 md:py-24 lg:py-32 xl:py-48">
-        <View className="px-4 md:px-6">
-          <View className="flex flex-col items-center gap-4 text-center">
-            <Text
-              role="heading"
-              className="text-3xl text-center native:text-5xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl"
-            >
-              Welcome to Project ACME
-            </Text>
-            <Text className="mx-auto max-w-[700px] text-lg text-center text-gray-500 md:text-xl dark:text-gray-400">
-              Discover and collaborate on acme. Explore our services now.
-            </Text>
+    <View className="">
+      <View className="flex flex-col items-center gap-8 ">
+        <Image source={banner1} className="object-cover w-full h-[500px] " />
 
-            <View className="gap-4">
-              <Link
-                suppressHighlighting
-                className="flex h-9 items-center justify-center overflow-hidden rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 web:shadow ios:shadow transition-colors hover:bg-gray-900/90 active:bg-gray-400/90 web:focus-visible:outline-none web:focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                href="/"
-              >
-                Explore
-              </Link>
-            </View>
-          </View>
+        <View className="px-4 space-y-10">
+          <Text
+            role="heading"
+            className=" text-5xl text-green-950 text-center "
+          >
+            Spices and Seasoning
+          </Text>
+          <Text className="text-green-950/50 text-center text-lg py-4">
+            Add a burst of flavour to your dishes with our aromatic spices and
+            seasonings
+          </Text>
+          <Link
+            href="https://www.woteva.com.uk"
+            className="px-6 py-4 w-fit rounded-lg bg-green-950"
+          >
+            <Text className="text-white text-lg text-center">Explore</Text>
+          </Link>
         </View>
       </View>
     </View>
@@ -49,30 +48,13 @@ function Header() {
   const { top } = useSafeAreaInsets();
   return (
     <View style={{ paddingTop: top }}>
-      <View className="px-4 lg:px-6 h-14 flex items-center flex-row justify-between ">
-        <Link className="font-bold flex-1 items-center justify-center" href="/">
-          ACME
-        </Link>
-        <View className="flex flex-row gap-4 sm:gap-6">
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            About
-          </Link>
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            Product
-          </Link>
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            Pricing
-          </Link>
-        </View>
+      <View className="px-4 lg:px-6 py-2 bg-green-600 flex items-center flex-row justify-center ">
+        <Text className="text-white text-center text-xs ">
+          Welcome to Woteva
+        </Text>
+      </View>
+      <View className="px-4 lg:px-6 h-14 bg-green-950 flex items-center flex-row justify-between ">
+        <Image source={logo} className="object-contain w-20 h-10" />
       </View>
     </View>
   );
@@ -87,7 +69,7 @@ function Footer() {
     >
       <View className="py-6 flex-1 items-start px-4 md:px-6 ">
         <Text className={"text-center text-gray-700"}>
-          © {new Date().getFullYear()} Me
+          © {new Date().getFullYear()} Woteva
         </Text>
       </View>
     </View>
