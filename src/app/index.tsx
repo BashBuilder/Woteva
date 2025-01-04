db1632f85a0d88ee94e1c4fb8328bfb6af2de00c
unknown <iamadelowotan@gmail.com>
2025-01-04 17:49:16 +0100

    update: carousel added

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,48 @@
 import { Link } from "expo-router";
-import React from "react";
-import { Image, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import logo from "../assets/woteva.png";
 import banner1 from "../assets/top-view-delicious-groceries-paper-bag - Copy 1.jpg";
+// import banner2 from "../assets/rice-that-has-been-peeled-is-placed-with-paddy-white.jpg";
+import banner3 from "../assets/frozen-food-table-arrangement.jpg";
+import banner4 from "../assets/skin care.jpg";
+import banner5 from "../assets/spicy-mango-salsa-featured-image.jpg";
+import banner6 from "../assets/household.jpg";
+
+const carouselData = [
+  {
+    title: "Spices and Seasoning",
+    description:
+      " Add a burst of flavour to your dishes with our aromatic spices and seasonings",
+    image: banner1,
+    button: "Explore",
+  },
+  {
+    title: "Taste the convenience",
+    description: "Shop your favourite food online",
+    image: banner3,
+    button: "Explore",
+  },
+  {
+    title: "Taste the convenience",
+    description: "Shop your favourite food online",
+    image: banner4,
+    button: "Explore",
+  },
+  {
+    title: "Taste the convenience",
+    description: "Shop your favourite food online",
+    image: banner5,
+    button: "Explore",
+  },
+  {
+    title: "Taste the convenience",
+    description: "Shop your favourite food online",
+    image: banner6,
+    button: "Explore",
+  },
+];
 
 export default function Page() {
   return (
@@ -16,31 +55,29 @@ export default function Page() {
 }
 
 function Content() {
-  return (
-    <View className="">
-      <View className="flex flex-col items-center gap-8 ">
-        <Image source={banner1} className="object-cover w-full h-[500px] " />
+  const [currentItem, setCurrentItem] = useState(0);
 
-        <View className="px-4 space-y-10">
-          <Text
-            role="heading"
-            className=" text-5xl text-green-950 text-center "
-          >
-            Spices and Seasoning
-          </Text>
-          <Text className="text-green-950/50 text-center text-lg py-4">
-            Add a burst of flavour to your dishes with our aromatic spices and
-            seasonings
-          </Text>
-          <Link
-            href="https://www.woteva.com.uk"
-            className="px-6 py-4 w-fit rounded-lg bg-green-950"
-          >
-            <Text className="text-white text-lg text-center">Explore</Text>
-          </Link>
-        </View>
-      </View>
-    </View>
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentItem((prevIndex) => (prevIndex + 1) % carouselData.length);
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (currentItem > carouselData.length) {
+    setCurrentItem(0);
+  }
+
+  return (
+    <ScrollView>
+      <Carousel
+        title={carouselData[currentItem].title}
+        description={carouselData[currentItem].description}
+        image={carouselData[currentItem].image}
+        buttonText={carouselData[currentItem].button}
+      />
+    </ScrollView>
   );
 }
 
@@ -55,6 +92,29 @@ function Header() {
       </View>
       <View className="px-4 lg:px-6 h-14 bg-green-950 flex items-center flex-row justify-between ">
         <Image source={logo} className="object-contain w-20 h-10" />
+      </View>
+    </View>
+  );
+}
+
+function Carousel({ title, image, description, buttonText }) {
+  return (
+    <View className="flex flex-col items-center gap-8 ">
+      <Image source={image} className="object-cover w-full h-[500px] " />
+
+      <View className="px-4 space-y-10">
+        <Text role="heading" className=" text-5xl text-green-950 text-center ">
+          {title}
+        </Text>
+        <Text className="text-green-950/50 text-center text-lg py-4">
+          {description}
+        </Text>
+        <Link
+          href="https://woteva.co.uk/"
+          className="px-6 py-4 w-fit rounded-lg bg-green-950"
+        >
+          <Text className="text-white text-lg text-center">{buttonText}</Text>
+        </Link>
       </View>
     </View>
   );
